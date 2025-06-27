243c9ab65c8ef0a1fc6c40d6fe7f00c6270eef63
Ottie3108@outlook.com <Ottie3108@outlook.com>
2025-06-27 16:49:02 +0200

    Chatfunctie toegepast

---
+++ Mobile deliveryapplication/Models/BerichtMarginConverter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+using System.Globalization;
+
+namespace Mobile_deliveryapplication.Models
+{
+    public class BerichtMarginConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isUser = (bool)value;
+            return isUser ? new Thickness(50, 5, 0, 5) : new Thickness(0, 5, 50, 5);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
